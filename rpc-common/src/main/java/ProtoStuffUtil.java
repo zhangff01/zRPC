7686434544fdce7e048a1a57b24d22e07cf0caf1
张飞飞 <zhangfeifei@souche.com>
2019-12-17 11:10:09 +0800

    feat:init

---
+++ rpc-common/src/main/java/ProtoStuffUtil.java
@@ -0,0 +1,53 @@
+import io.protostuff.LinkedBuffer;
+import io.protostuff.ProtostuffIOUtil;
+import io.protostuff.Schema;
+import io.protostuff.runtime.RuntimeSchema;
+import lombok.Data;
+
+/**
+ * @author zhangfeifei
+ * @Description protostuff 序列化&反序列化工具
+ * @create 2019/12/17
+ */
+@Data
+@SuppressWarnings("unchecked")
+public class ProtoStuffUtil {
+    /**
+     * 避免每次序列化都重新申请Buffer空间
+     */
+    private static LinkedBuffer buffer = LinkedBuffer.allocate(LinkedBuffer.DEFAULT_BUFFER_SIZE);
+
+    /**
+     * 序列化方法，把指定对象序列化成字节数组
+     *
+     * @param obj
+     * @param <T>
+     * @return
+     */
+    public static <T> byte[] serialize(T obj) {
+        Class<T> clazz = (Class<T>) obj.getClass();
+        Schema<T> schema = RuntimeSchema.getSchema(clazz);
+        byte[] data;
+        try {
+            data = ProtostuffIOUtil.toByteArray(obj, schema, buffer);
+        } finally {
+            buffer.clear();
+        }
+        return data;
+    }
+
+    /**
+     * 反序列化方法，将字节数组反序列化成指定Class类型
+     *
+     * @param data
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T> T deserialize(byte[] data, Class<T> clazz) {
+        Schema<T> schema = RuntimeSchema.getSchema(clazz);
+        T obj = schema.newMessage();
+        ProtostuffIOUtil.mergeFrom(data, obj, schema);
+        return obj;
+    }
+}
